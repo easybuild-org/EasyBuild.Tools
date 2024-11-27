ecd638edc38be38a3247aa7891a762c92e8ea973
Maxime Mangel <mangel.maxime@protonmail.com>
2024-11-27 21:58:52 +0100

    feat: add `Npm.publish`

---
+++ src/Npm.fs
@@ -0,0 +1,24 @@
+module EasyBuild.Tools.Npm
+
+open SimpleExec
+open System.IO
+open BlackFox.CommandLine
+
+type Npm =
+
+    static member publish(projectDir: string, ?tag: string, ?isRestricted: bool) =
+        let access =
+            match isRestricted with
+            | Some true -> "restricted"
+            | Some false
+            | None -> "public"
+
+        Command.Run(
+            "npm",
+            CmdLine.empty
+            |> CmdLine.appendRaw "publish"
+            |> CmdLine.appendPrefixIfSome "--tag" tag
+            |> CmdLine.appendPrefix "--access" access
+            |> CmdLine.toString,
+            workingDirectory = projectDir
+        )
