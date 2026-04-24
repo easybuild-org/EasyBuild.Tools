5eac5cd59ec61f130434d39f96a8125820f89c27
Mangel Maxime <me@mangelmaxime.fr>
2026-04-24 21:01:22 +0200

    feat!: don't install Huksy hooks if `ACT` env variable is detected

--- src/Husky.fs
+++ src/Husky.fs
@@ -1,13 +1,23 @@
 module EasyBuild.Tools.Husky
 
 open SimpleExec
+open System
 
 type Husky =
 
+    /// <summary>
+    /// Install Husky hooks unless it detects `ACT` env variable.
+    ///
+    /// This is because if the person is using `act` with worktree then Husky fails to install
+    /// </summary>
     static member install
         // begin-snippet: Husky.install
         (?workingDirectory: string)
         : unit
         // end-snippet
         =
-        Command.Run("dotnet", "husky install", ?workingDirectory = workingDirectory)
+
+        if Environment.GetEnvironmentVariable("ACT") <> null then
+            ()
+        else
+            Command.Run("dotnet", "husky install", ?workingDirectory = workingDirectory)
